9fcd27d8fda6568f45527f67a529f374eb7b75ca
Peter Prikryl <peto.prikryl@gmail.com>
2020-11-23 01:32:16 -0800

    Correct whitespace settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,44 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/member-delimiter-style": [
-            "warn",
-            {
-                "multiline": {
-                    "delimiter": "semi",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "@typescript-eslint/naming-convention": "warn",
-        "@typescript-eslint/no-unused-expressions": "warn",
-        "@typescript-eslint/semi": [
-            "warn",
-            "always"
-        ],
-        "curly": "warn",
-        "eqeqeq": [
-            "warn",
-            "always"
-        ],
-        "no-redeclare": "warn",
-        "no-throw-literal": "warn"
-    }
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"project": "tsconfig.json",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"@typescript-eslint/member-delimiter-style": [
+			"warn",
+			{
+				"multiline": {
+					"delimiter": "semi",
+					"requireLast": true
+				},
+				"singleline": {
+					"delimiter": "semi",
+					"requireLast": false
+				}
+			}
+		],
+		"@typescript-eslint/naming-convention": "warn",
+		"@typescript-eslint/no-unused-expressions": "warn",
+		"@typescript-eslint/semi": [
+			"warn",
+			"always"
+		],
+		"indent": ["error", "tab", { "SwitchCase": 1 }],
+		"curly": "warn",
+		"eqeqeq": [
+			"warn",
+			"always"
+		],
+		"no-trailing-spaces": "error",
+		"no-redeclare": "warn",
+		"no-throw-literal": "warn"
+	}
 };
